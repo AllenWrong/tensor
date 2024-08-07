f6e3abd2a7c6816b8f40adba25c189347e70eec2
zhongchao.guan <zhongchao.guan@kunlun-inc.com>
2024-08-07 11:18:54 +0800

    update: add tensornd

---
+++ tensornd.h
@@ -0,0 +1,47 @@
+#ifndef TENSORND_H
+#define TENSORND_H
+
+#include <stdlib.h>
+
+typedef struct {
+    float* data;
+    size_t data_size;
+    int ref_count;
+} Storage;
+
+typedef struct {
+    Storage* storage;
+    int ndim;
+    int* size;
+    int* offset;
+    int* stride;
+    char* repr;
+} Tensor;
+
+Tensor* tensor_empty(int* size, int ndim);
+size_t nelement(Tensor* t);
+void tensor_copy_np(Tensor* t, float* data);
+size_t logical_to_physical(Tensor* t, int* idx);
+float tensor_getitem(Tensor* t, int* idx);
+Tensor* tensor_slice_keepdim(Tensor* t, int* start, int* end, int* step);
+Tensor* tensor_slice(Tensor* t, int* start, int* end, int* step);
+float tensor_item(Tensor* t);
+int* shape(Tensor* t);
+int ndim(Tensor* t);
+
+// Tensor* tensor_getitem_astensor(Tensor* t, int* index);
+
+// void tensor_setitem(Tensor* t, int* index, float val);
+// void tensor_slice_set_item(Tensor* t, index* index, Tensor* t);
+
+// Tensor* tensor_addf(Tensor* t, float val);
+// Tensor* tensor_add(Tensor* t1, Tensor* t2);
+
+void tensor_incref(Tensor* t);
+void tensor_decref(Tensor* t);
+void tensor_free(Tensor* t);
+
+char* tensor_to_string(Tensor* t);
+// void tensor_print(Tensor* t);
+
+#endif
